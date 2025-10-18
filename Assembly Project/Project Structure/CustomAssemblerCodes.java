93d0e2d374150a14af0c5a8eb242f7cd458185c0
SkylerAndersen <skylerandersen23@gmail.com>
2025-10-18 13:38:00 +0200

    Moved Assembly Project to its own repository with releases and source.

--- Assembly Project/Project Structure/CustomAssemblerCodes.java
+++
@@ -1,7 +0,0 @@
-enum CustomAssemblerCodes {
-    ITEM("ORDER","BINARY"),
-    ITEM2("ORDER","BINARY");
-    private final String binary;
-    private final String order;
-    CustomAssemblerCodes(String order, String binary) {}
-}
--- Assembly Project/Project Structure/Main.java
+++
@@ -1,6 +0,0 @@
-public class Main {
-    public static void main (String[] args) {
-//        CustomInterpreter interpreter = new CustomInterpreter();
-//        interpreter.run();
-    }
-}
